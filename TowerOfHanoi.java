2f093095cb5dd10d8e54960ebb83c93f3dadf209
n2phoenix <nirajmmenon@gmail.com>
2015-09-21 09:25:15 +0400

    Added BufferedWriter and FileWriter classes

--- TowerOfHanoi.java
+++ TowerOfHanoi.java
@@ -1,11 +1,13 @@
 import java.util.*;
+import java.io.*;
 public class TowerOfHanoi 
 {
     int n;
     String a;
     String c;
     String b;
-
+    static FileWriter fout;
+    static PrintWriter pout;
     public TowerOfHanoi(int n, String a, String c, String b) 
     {
         this.n = n;
@@ -14,7 +16,7 @@ public class TowerOfHanoi
         this.b = b;
     }
 
-    public void solve() 
+    public void solve() throws IOException
     {
         System.out.println("Steps for solving " + n + " disk tower of hanoi puzzle: ");
         System.out.println("starting with discs on " + a + " and moving discs to " + c + ":");
@@ -24,16 +26,16 @@ public class TowerOfHanoi
         solveTower(n, a, c, b);   
     }
 
-    // Recursive helper method that prints the instructions for
-    // moving numDisks from the a to the c using the
-    // b.
-    public void solveTower(int num, String from, String to, String using) 
+    public void solveTower(int num, String from, String to, String using) throws IOException
     {
-        if (num == 1) {
+        if (num == 1) 
+        {
             // Base Case: Move 1 disk...
             System.out.println ("Move disk from " + from + " to " + to + ".");
+            pout.println ("Move disk from " + from + " to " + to + ".");
         }
-        else {
+        else 
+        {
             //   Recursive Case: 
             //   Move num-1 disks from from a to b using c.
             solveTower(num - 1, from, using, to);   
@@ -46,12 +48,16 @@ public class TowerOfHanoi
         }
     }
 
-    public static void main() 
+    public static void main() throws IOException
     {
         Scanner in = new Scanner (System.in);
         System.out.println ("Enter number of disks: ");
         int x = Integer.parseInt (in.nextLine());
         TowerOfHanoi tower = new TowerOfHanoi(x, "A", "B", "C");
+        tower.fout = new FileWriter ("the-file-name2.txt");
+        tower.pout = new PrintWriter (new BufferedWriter (fout));
+        tower.pout.close();
+        tower.fout.close();
         tower.solve();                                   
     }
 }
