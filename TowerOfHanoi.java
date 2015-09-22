05c0bba29fc6aae6165123332618e4f09b7e9078
n2phoenix <nirajmmenon@gmail.com>
2015-09-22 11:21:20 +0400

    Bit more changes...

--- TowerOfHanoi.java
+++ TowerOfHanoi.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-public class TowerOfHanoi 
+public class TowerOfHanoi
 {
     int n;
     String a;
@@ -8,7 +8,7 @@ public class TowerOfHanoi
     String b;
     static FileWriter fout;
     static PrintWriter pout;
-    public TowerOfHanoi(int n, String a, String c, String b) 
+    public TowerOfHanoi(int n, String a, String c, String b)
     {
         this.n = n;
         this.a = a;
@@ -19,32 +19,32 @@ public class TowerOfHanoi
     public void solve() throws IOException
     {
         System.out.println("Steps for solving " + n + " disk tower of hanoi puzzle: ");
+        pout.println ("Steps for solving " + n + " disk tower of hanoi puzzle: ");
         System.out.println("starting with discs on " + a + " and moving discs to " + c + ":");
+        pout.println ("starting with discs on " + a + " and moving discs to " + c + ":");
         System.out.println();
-
+        pout.println();
         // Solve the problem by moving n disks from a to c using b.
-        solveTower(n, a, c, b);   
+        solveTower(n, a, c, b);
     }
 
     public void solveTower(int num, String from, String to, String using) throws IOException
     {
-        if (num == 1) 
+        if (num == 1)
         {
             // Base Case: Move 1 disk...
             System.out.println ("Move disk from " + from + " to " + to + ".");
             pout.println ("Move disk from " + from + " to " + to + ".");
         }
-        else 
+        else
         {
-            //   Recursive Case: 
-            //   Move num-1 disks from from a to b using c.
-            solveTower(num - 1, from, using, to);   
-
-            //   Move 1 disk from the a to the c using b.
-            solveTower(1, from, to, using);            
-
-            //   Move num-1 disks from b to c using a.
-            solveTower(num - 1, using, to, from); 
+            // Recursive Case:
+            // Move num-1 disks from from a to b using c.
+            solveTower(num - 1, from, using, to);
+            // Move 1 disk from the a to the c using b.
+            solveTower(1, from, to, using);
+            // Move num-1 disks from b to c using a.
+            solveTower(num - 1, using, to, from);
         }
     }
 
@@ -54,10 +54,9 @@ public class TowerOfHanoi
         System.out.println ("Enter number of disks: ");
         int x = Integer.parseInt (in.nextLine());
         TowerOfHanoi tower = new TowerOfHanoi(x, "A", "B", "C");
-        tower.fout = new FileWriter ("the-file-name2.txt");
+        tower.fout = new FileWriter ("C:/Users/niraj/Desktop/TOH.txt");
         tower.pout = new PrintWriter (new BufferedWriter (fout));
+        tower.solve();
         tower.pout.close();
-        tower.fout.close();
-        tower.solve();                                   
     }
 }
